44609ce4be57e5076d33a69ce1c45eac21a3e84f
you <you@gmail.com>
2025-11-14 09:39:39 +0200

    history aint workin, appendChild problem at fault

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 inputField = document.getElementById("inputField")
 button = document.getElementById("guess")
+random = document.getElementById("random")
 textElement = document.getElementById("texty")
+historye = document.getElementById("history")
+haveGuessed = 0;
+guessCount = 0;
 
 function getRandomInt(min, max) {
   const minCeiled = Math.ceil(min);
@@ -11,8 +15,19 @@ function getRandomInt(min, max) {
 guessValue = getRandomInt(1, 100)
 
 
+random.addEventListener("click",function(){
+    if (haveGuessed == 1){
+        haveGuessed = 0;
+    }
+    guessCount = 0;
+
+    guessValue = getRandomInt(1, 100)
+}
+)
+
 button.addEventListener("click", function(){
     inputValue = inputField.value;
+    guessCount = guessCount + 1;
     if(inputValue >= 1){
         if(inputValue <= 100){
             if(inputValue > guessValue){
@@ -20,7 +35,16 @@ button.addEventListener("click", function(){
             } else if(inputValue < guessValue){
                 textElement.innerHTML = "Your guess was too low!";
             } else if (inputValue == guessValue){
-                textElement.innerHTML = "Congratulations you've guessed the number!";
+                textElement.innerHTML = "Congratulations you've guessed the number within " + guessCount +" guesses! You can continiue by pressing 'Generate new number'!";
+                haveGuessed = 1;
+
+                document.historye.appendChild(latestHistory = document.createElement("li"));
+                document.latestHistory.appendChild(historyText = document.createElement("p"));
+                
+
+                historyText.innerHTML = "The number here was " + guessValue + " and you guessed it, with " + guessCount + " guesses.";
+
+                
             } else {
                 textElement.innerHTML = "Something went wrong!";
             }
@@ -32,3 +56,4 @@ button.addEventListener("click", function(){
     }
 }
 )
+
